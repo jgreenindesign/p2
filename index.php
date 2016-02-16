1abe94ed9cc66643b9ffff5dad7ae3ec6b97e07d
James Green <jamesgreen@mail.usf.edu>
2016-02-15 21:24:42 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <title>James Green - P2</title>
+
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+
+
+        <?php require('logic.php'); ?>
+
+    </head>
+    <body>
+
+    </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,15 @@
+# CSCI E-15 Project 2 : Create an xkcd style password generator.
+
+## Live URL
+<http://p2.jgreenindesign.com/>
+
+## Description
+
+
+## Video URL
+
+
+## Additional Info to TA
+
+
+## Additional Resources
